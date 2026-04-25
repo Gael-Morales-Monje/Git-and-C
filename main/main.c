54b4191cfec001357ef618067ea837dabe11504b
Gael-Morales-Monje <gaelmoralesmonje@gmail.com>
2026-04-25 00:08:42 -0600

    prueba1

---
+++ main/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+#include<string.h>
+
+int main() {
+
+    FILE *file;
+
+    file = fopen("../text/text.txt","r");
+
+    char letra[8];
+
+    fscanf(file,"%s",letra);
+
+
+    fclose(file);
+
+
+
+    printf("----%s",letra);
+
+
+    return 0;
+}
